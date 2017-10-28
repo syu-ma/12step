3ef715eb71af21b40ac32ad8e6a4424714c6a424
Shingo Masuda <sin.five.m@gmail.com>
2017-10-29 04:12:46 +0900

    Mapping data and bss from ROM to RAM and check operation.

--- 03/bootload/main.c
+++ 03/bootload/main.c
@@ -2,16 +2,46 @@
 #include "serial.h"
 #include "lib.h"
 
-volatile int value = 10;
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
 
-int main(void){
-	serial_init(SERIAL_DEFAULT_DEVICE);
+static void printval(void)
+{
+  puts("global_data = "); putxval(global_data,0); puts("\n");
+  puts("global_bss = ");  putxval(global_bss,0);  puts("\n");
+  puts("static_data = "); putxval(static_data,0); puts("\n");
+  puts("static_bss = ");  putxval(static_bss,0);  puts("\n");
+}
 
-	puts("Hello World!\n");
+static int init(void)
+{
+  extern  int erodata,  data_start, edata,  bss_start, ebss;
+  
+  /* Initialize a data segment and bss segment.
+   * Please care that global variables are not abailable after this init.
+   */
+  memcpy(&data_start, &erodata, (long)&edata  - (long)&data_start);
+  memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
 
-  putxval(value,0);  puts("\n");
-  value = 20;
-  putxval(value,0);  puts("\n");
+  /* Initialize serial */
+  serial_init(SERIAL_DEFAULT_DEVICE);
+  
+  return 0;
+}
+
+  int main(void){
+	init();
+  puts("Hello World!\n");
+
+  printval();
+  puts("overwrite variables \n");
+  global_data = 0x20;
+  global_bss   = 0x30;
+  static_data = 0x40;
+  static_bss  = 0x50;
+  printval();
 
 	while(1)
 	;
